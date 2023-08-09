8f9e6ab6ece42170492ae137eaabaf4a32aa9ac9
Madafakki <josa.morales@duocuc.cl>
2023-08-09 13:47:27 -0400

    Solo para recordar como usar IONIC, nada que ver en mi proyecto NODE.JS

--- 1.os-info.js
+++ 1.os-info.js
@@ -1,11 +1,24 @@
 const os = require('node:os')
 
-console.log('Informacion del sistema operativo:')
-console.log('--------------------')
-console.log('nombre del sistema operativo: ', os.platform())
-console.log('version del sistema operativo: ', os.release())
-console.log('Arquitectura : ', os.arch());
-console.log('Cpus', os.cpus()) // vamos a poder escalar procesos en node
-console.log('Memoria libre : ', os.freemem() / 1024 / 1024);
-console.log('Memoria total : ', os.totalmem() / 1024 / 1024);
-console.log('Tiempo encendido: ', os.uptime() / 60 / 60)
+// console.log('Informacion del sistema operativo:')
+// console.log('--------------------')
+// console.log('nombre del sistema operativo: ', os.platform())
+// console.log('version del sistema operativo: ', os.release())
+// console.log('Arquitectura : ', os.arch());
+// console.log('Cpus', os.cpus()) // vamos a poder escalar procesos en node
+// console.log('Memoria libre : ', os.freemem() / 1024 / 1024);
+// console.log('Memoria total : ', os.totalmem() / 1024 / 1024);
+// console.log('Tiempo encendido: ', os.uptime() / 60 / 60)
+
+function SUMLOOP(times) {
+    let list = [423, 791, 116, 313, 1049, 504, 194, 219, 322, 1197, 184, 1251, 792, 1004, 330, 372, 884, 818, 1014, 428, 188, 529, 187, 1257, 55, 537, 377, 344, 1292, 155, 347, 602]
+
+    let sumloop = 0
+
+    for (let index = 0; index < times; index++) {
+        sumloop += list[index];
+    }
+    console.log(sumloop)
+}
+
+SUMLOOP(32);
--- README.md
+++ README.md
@@ -22,3 +22,15 @@ Requisitos para saber usar nodejs:
 Para usar el entorno fnm e instalar la version de node que quiera hacer primero tengo que usar fnm y mandar este code: 
 fnm env --use-on-cd | Out-String | Invoke-Expression
 despues de eso puedo usar tranquilamente node (creoxd)
+
+
+En node js no tendremos todo en un solo fichero (archivo)
+Se utilizara el patron de diseño modulo = Se separaran el codigo en distintos ficheros, donde podras importar y exportar para no repetir codigo entre otros.
+
+
+
+<!--? PARA HACER LA INSTALACIÓN DE IONIC EN NODE.JS -->
+Tienes que usar lo mismo que hacias en node.js de invoke-expression etc, usando la que tenemos de default y luego usando el npm, si quieres actualizas npm instalandolo.  PS C:\Users\jsmsp> npm install -g npm@9.8.1
+Finalmente si te vuelve salir error al instalar debes usar en el powershell como administrador : Set-ExecutionPolicy Unrestricted
+IONIC debes usar npm install -g @ionic/cli
+
